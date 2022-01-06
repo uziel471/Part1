52041a37f6af0ef02ea7883682cfdb4be863d143
Uziel Estrada <uziel41@gmail.com>
2022-01-05 23:08:23 -0800

    practica 1 pasar a componentes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import './App.css';
-import Mensaje from './Mensaje'
+import React from 'react'
 
-const Description = () => {
-  return <p>
-    Esta es la app del curso fullstack bootcamp
-  </p>
+const Title = ({tittle}) => <h1>{tittle}</h1>
+
+const Notas = ({materia, calificacion}) => {
+  return <p>{materia} {calificacion}</p>
 }
+
+const TotalCalificacion = (props) => {
+  const { exercises1, exercises2, exercises3 } = props;
+  return <p> Number of exercises {exercises1 + exercises2 + exercises3} </p>
+} 
 
 const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
   return (
-    <div className="App">
-      <Mensaje color='red' message='Estamos trabajando' />
-      <Mensaje color='green' message='En un curso'/>
-      <Mensaje color='yellow' message='De React'/>
-      <Description />
+    <div>
+      <Title tittle={course} />
+      <Notas materia={part1} calificacion={exercises1} />
+      <Notas materia={part2} calificacion={exercises2} />
+      <Notas materia={part3} calificacion={exercises3} />
+      <TotalCalificacion 
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
